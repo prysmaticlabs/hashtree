fac5b98e5cade103104acaa5bec0c5ae24b1a016
Potuz <potuz@prysmaticlabs.com>
2023-05-11 17:05:08 -0300

    Detect cpu on arm on tests

--- src/test.c
+++ src/test.c
@@ -25,6 +25,10 @@
 #ifdef __x86_64__
 #include <cpuid.h>
 #endif
+#ifdef __aarch64__
+#include <sys/auxv.h>
+#include <asm/hwcap.h>
+#endif
 #include "acutest.h"
 #include "hashtree.h"
 
@@ -585,6 +589,8 @@ void test_armv8_neon_x4() {
 }
 
 void test_hash_armv8_crypto_multiple_blocks() {
+    long hwcaps = getauxval(AT_HWCAP);
+    if (hwcaps & HWCAP_SHA2) {
     unsigned char digest[128];
 
     sha256_armv8_crypto(digest, test_16_block, 4);
@@ -596,6 +602,11 @@ void test_hash_armv8_crypto_multiple_blocks() {
     TEST_CHECK(digests_equal(digest, test_4_digests, sizeof(digest)));
     TEST_DUMP("Expected: ", test_4_digests, sizeof(test_4_digests));
     TEST_DUMP("Produced: ", digest, sizeof(digest));
+    } else {
+        acutest_colored_printf_(ACUTEST_COLOR_GREEN_INTENSIVE_, "[ CPU does not support Sha2 instructions ]\n");
+        acutest_test_failures_ = 1;
+    }
+
 }
 #endif
 
